826e97ef30cbd6a00283e426eb294afa71eb7b48
fadawar <fadawar@gmail.com>
2017-02-06 13:32:59 +0100

    Add whole application

---
+++ README.md
@@ -0,0 +1,7 @@
+# Leaflet Webmaps with Python and Folium
+
+_Application 2 from The Python Mega Course_
+
+Show Volcanoes on map. Also population of countries.
+
+![Screenshot](screenshot.png)
---
+++ app.py
@@ -0,0 +1,31 @@
+import folium
+import pandas
+
+
+def color(elev):
+    minimum = int(min(df['ELEV']))
+    step=int((max(df['ELEV'])-min(df['ELEV']))/3)
+    if elev in range(minimum,minimum+step):
+        return 'green'
+    elif elev in range(minimum+step,minimum+step*2):
+        return 'orange'
+    else:
+        return 'red'
+
+
+df = pandas.read_csv("Volcanoes-USA.txt")
+
+map = folium.Map(location=[df['LAT'].mean(),df['LON'].mean()],zoom_start=5,tiles='Stamen Terrain')
+
+fg = folium.FeatureGroup(name='Volcano Locations')
+
+for lat,lon,name,elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
+    fg.add_child(folium.Marker(location=[lat,lon], popup=name, icon=folium.Icon(color=color(elev))))
+
+map.add_child(fg)
+
+map.add_child(folium.GeoJson(data=open('world_population.geojson'), name='World Population', style_function=lambda x: {'fillColor': 'green' if x['properties']['POP2005']<=10000000 else 'orange' if 10000000<x['properties']['POP2005']<20000000 else 'red' }))
+
+map.add_child(folium.LayerControl())
+
+map.save(outfile='test.html')
